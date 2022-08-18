35c5327a65d15886fff95b5c65a7eb783693d36b
Divyansh Juneja <divyansh.juneja@gmail.com>
2022-08-18 15:35:51 +0530

    Version basic completed

--- Desktop/deshIndex/index.js
+++ Desktop/deshIndex/index.js
@@ -5,9 +5,13 @@ var { engine } = require('express-handlebars');
 const app = express();
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 // const request = require('request');
 // const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set handleBars Middleware
 //Create call_Api function
@@ -17,9 +21,11 @@ app.use(express.static('views'));
 
 //API key
 // pk_cab7e99d49134657bfbc0545f85a6d8a
-function call_Api(finishedAPI) {
+function call_Api(finishedAPI, ticker) {
   request(
-    'https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_cab7e99d49134657bfbc0545f85a6d8a',
+    'https://cloud.iexapis.com/stable/stock/' +
+      ticker +
+      '/quote?token=pk_cab7e99d49134657bfbc0545f85a6d8a',
     { json: true },
     (err, res, body) => {
       if (err) {
@@ -34,15 +40,28 @@ function call_Api(finishedAPI) {
   );
 }
 
-// Set Handlebar Routes
+// Set Handlebar Index GET Routes
 app.get('/', (req, res) => {
   //   const api = call_Api();
   call_Api(function (done_Api) {
     res.render('home', {
       stock: done_Api,
     });
-  });
+  }, 'fb');
 });
+
+// Set Handlebar Index POST Routes
+app.post('/', (req, res) => {
+  //   const api = call_Api();
+  call_Api(function (done_Api) {
+    posted_Stuff = req.body.stock_ticker;
+    res.render('home', {
+      stock: done_Api,
+      //   posted_Stuff: posted_Stuff,
+    });
+  }, req.body.stock_ticker);
+});
+
 //about page
 app.get('/about', (req, res) => {
   res.render('about');
